9e0fff3aa4b22dfc5ab1ba940b748b379ee011fd
builditwithdivya <divydshar@gmail.com>
2025-04-25 11:56:07 +0530

    Delete 2.6.py

--- 2.6.py
+++
@@ -1,9 +0,0 @@
-number = input("Please enter a number: ")
-
-number = number.strip()
-
-if number.isdigit() or (number[0] == '-' and number[1:].isdigit()):
-    num_digits = len(number) if number[0] != '-' else len(number) - 1
-    print(f"The number of digits in {number} is: {num_digits}")
-else:
-    print("Invalid input. Please enter a valid integer.")
